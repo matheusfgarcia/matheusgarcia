e3e1dc2b75ec5e940b646532bd342c3316014fcb
matheus garcia <me@matheusgarcia.com>
2016-04-27 00:30:38 -0300

    first commit

--- header.php
+++ header.php
@@ -1,12 +1,4 @@
 
-<head>
-    <title>
-        Web Development - Matheus Garcia
-    </title>
-
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
-</head>
-
 <header>
 
 
--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
 
+require('head.php');
+
+?>
+
+
+<body>
+
+<?php
+
 require('header.php');
 
+?>
+
+<div class="content">
+
+</div>
+
+
+<?php
 
 require('footer.php');
+
+?>
+
+</body>
